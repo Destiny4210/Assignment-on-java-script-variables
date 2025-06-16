eed7f6df28980d67aaad58b790f0545f50871829
Ricky <destinypatrick4772@gmail.com>
2025-06-16 14:26:03 +0100

    Add files via upload

---
+++ Java script assignment on variables/index.js
@@ -0,0 +1,43 @@
+
+
+const firstName = "Destiny"
+
+const lastName = "Patrick"
+
+const brotherName = "Wisdom"
+
+const cousinName = "Salvation"
+
+const mumName = "Mirian"
+
+console.log(firstName.length)
+
+console.log(lastName.toLowerCase())
+
+console.log(brotherName.includes("W"))
+
+console.log(cousinName.endsWith("n"))
+
+console.log(mumName.charAt("1"))
+
+const age = 20
+
+const score = 90
+
+const height = 175
+
+const weight = 65
+
+const price = 100
+
+const quantity = 5
+
+const isAgeVerified = true
+
+const hasHighScore = true
+
+const isOverweight = false
+
+const isHeightAverage = true
+
+const isQuantityAvailabe = false
